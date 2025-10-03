a53e05496410a4a2f4ab7399c420a90b6758b05a
Paki <pasqualecriscuolo2010@gmail.com>
2025-10-03 21:42:51 +0200

    Cleaned

--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
+# NAMESPACES
 variable "namespace" {
   description = "The name of the Kubernetes namespace"
   type        = string
@@ -5,27 +6,22 @@ variable "namespace" {
 }
 
 
-
 # FRONTEND
-
 variable "frontend_deployment_name" {
   description = "The name of the Kubernetes deployment"
   type        = string
   default     = "tflab-frontend-deploy"
 }
-
 variable "frontend_image" {
   description = "The Docker image for the frontend"
   type        = string
   default     = "nginx:1.21.6"
 }
-
 variable "frontend_app_label" {
   description = "App label for Kubernetes resources"
   type        = string
   default     = "tflab-frontend-app"
 }
-
 variable "frontend_service_label" {
   description = "Service label for frontend service"
   type        = string
@@ -33,27 +29,22 @@ variable "frontend_service_label" {
 }
 
 
-
 # BACKEND
-
 variable "backend_deployment_name" {
   description = "The name of the Kubernetes deployment"
   type        = string
   default     = "tflab-backend-deploy"
 }
-
 variable "backend_image" {
   description = "The Docker image for the backend"
   type        = string
   default     = "paki13/tflab-backend:latest"
 }
-
 variable "backend_app_label" {
   description = "App label for Kubernetes resources"
   type        = string
   default     = "tflab-backend-app"
 }
-
 variable "backend_service_label" {
   description = "Service label for backend service"
   type        = string
